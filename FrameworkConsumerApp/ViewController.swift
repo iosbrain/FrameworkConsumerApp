b348b5fa6f5a3f80b8b1dca39ac64632fd158dbd
Andrew Jaffee <sales@microit.us>
2018-01-14 10:16:13 -0700

    Initial commit to new repo

---
+++ FrameworkConsumerApp/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  FrameworkConsumerApp
+//
+/* Copyright (c) 2018 Andrew Jaffee, microIT Infrastructure, LLC, and
+ iosbrain.com.
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ */
+
+import UIKit
+
+// Import the Swift 4 framework
+import SwiftFramework
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create an instance of a framework class.
+        let frameworkClass = FrameworkClass()
+        // Invoke a method of the framework class.
+        _ = frameworkClass.add(left: 8, right: 8)
+        // "8 + 8 = 16" will be written to console.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
